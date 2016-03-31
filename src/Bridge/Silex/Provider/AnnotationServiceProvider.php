b92192c47142d620b810b84cbe45d6fa0210c436
Nathan Craike <nathan@studionone.com.au>
2016-03-31 15:35:13 +1000

    Adds generic annotation loader and updates Silex bridge.
    This separates the annotation loading functionality (which is needed in any project) of the Silex bridge from the Silex-specific parts.
    
    The new Schale\AnnotationLoader class can be registered as a loader for Doctrine Annotations' registry, but doesn't contain any Silex-specific functionality.
    
    The remaining code in the Silex bridge's provider (AnnotationServiceProvider) uses this new AnnotationLoader class, and now just does the Silex-specific bits.

--- src/Bridge/Silex/Provider/AnnotationServiceProvider.php
+++ src/Bridge/Silex/Provider/AnnotationServiceProvider.php
@@ -3,11 +3,12 @@ namespace Schale\Bridge\Silex\Provider;
 
 use Silex\{Application, ServiceProviderInterface};
 use Doctrine\Common\Annotations\{AnnotationReader, AnnotationRegistry};
+use Schale\AnnotationLoader;
 
 /**
  * Register our annotations for use.
  *
- * This sets up our annotations system on ExampleApp init.
+ * This sets up our annotations system on Silex/Flint app init.
  *
  * We're using Doctrine\Common's annotation system. For documentation,
  * see http://doctrine-common.readthedocs.org/en/latest/reference/annotations.html
@@ -16,44 +17,44 @@ use Doctrine\Common\Annotations\{AnnotationReader, AnnotationRegistry};
  */
 class AnnotationServiceProvider implements ServiceProviderInterface
 {
+    protected $annotationLoader;
+    protected $annotationReader;
+
+    public function __construct(
+        array $annotationFqcnsAndFilePaths=[],
+        bool $includeBuiltInAnnotations=AnnotationLoader::INCLUDE_BUILTIN_ANNOTATIONS,
+        bool $annotationsMustBeLoadedExplicitly=AnnotationLoader::ANNOTATIONS_MUST_BE_LOADED_EXPLICITLY
+    ) {
+        $this->annotationLoader = new AnnotationLoader(
+            $annotationFqcnsAndFilePaths,
+            $includeBuiltInAnnotations,
+            $annotationsMustBeLoadedExplicitly);
+
+        $this->annotationReader = new AnnotationReader();
+    }
+
+    public function getAnnotationLoader(): AnnotationLoader
+    {
+        return $this->annotationLoader;
+    }
+
+    public function getAnnotationReader(): AnnotationReader
+    {
+        return $this->annotationReader;
+    }
+
     /**
-     * Registers our annotation setup. Currently hard-coded, but will be
-     * configurable in the future
-     * TODO: Make this configurable
+     * Registers the annotation loader in the Doctrine Annotations
+     * registry, and adds the annotation reader to the app.
      *
      * @param Silex\App $pimple
      */
     public function register(Application $app)
     {
-        /**
-         * registerLoader takes a callable that matches spl_autoload_register
-         * XXX: The callable **must** fail silently!
-         *
-         * Note that the callable may be called multiple times for an
-         * annotation: first with the non-fully-qualified name, and then with
-         * the fully-qualified name.
-         *
-         * The full-qualification of names may be derived based on "use"
-         * statements in the file where the annotation is used. eg if a class
-         * file has "use Schale\Annotation;", then Annotation\Model will be
-         * valid for annotating within that file.
-         */
-        AnnotationRegistry::registerLoader(function($baseClassname) {
-            $baseDir = __DIR__ . '/../../../Annotation';
-            $classname = ltrim($baseClassname, '\\');
+        AnnotationRegistry::registerLoader(
+            [$this->annotationLoader, 'load']);
 
-            // Checks we're using the correct FQCN
-            //
-            // This means we only load annotations which are fully qualified.
-            if (strpos($classname, 'Schale\Annotation\\') === 0) {
-                return true;
-            }
-
-            return false;
-        });
-
-        $reader = new AnnotationReader();
-        $app['annotation_reader'] = $reader;
+        $app['annotation_reader'] = $this->annotationReader;
     }
 
     public function boot(Application $app)

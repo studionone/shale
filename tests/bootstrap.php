7e3f1f7288f955d91b16d520aee5727d264e382d
Nathan Craike <nathan@studionone.com.au>
2016-04-01 14:40:23 +1000

    Removes dependency on Tracy Debugger for logging.
    This was only used in tests, and didn't provide much useful information.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,7 +3,3 @@
  * PHPUnit test-suite bootstrap
  */
 require_once(__DIR__ . '/../vendor/autoload.php');
-
-use Tracy\Debugger;
-Debugger:$strictMode = true;
-Debugger::enable(Debugger::DEVELOPMENT, __DIR__ . '/logs');
